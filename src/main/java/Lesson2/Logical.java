3f39b11f3607e003be9abfbff35e1bd9e7b73427
DanilKozlov00 <55684917+DanilKozlov00@users.noreply.github.com>
2020-11-08 23:35:13 +0300

    Lesson6 ✅

--- src/main/java/Lesson2/Logical.java
+++ src/main/java/Lesson2/Logical.java
@@ -41,52 +41,53 @@ public class Logical {
 
     /**
      * Простая
-     *
+     * <p>
      * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
      * Вернуть число дней в этом месяце этого года по григорианскому календарю.
      */
-    int daysInMonth(int month,int year) {
+    public int daysInMonth(int month, int year) {
         if (month == 2)
-             if (year % 400 == 0) {
-             return 29;
-        } else if (year % 100 == 0) {
-           return  28;
-        } else if (year % 4 == 0) {
-           return  29;
-        } else {
-           return 28;
-        }
-    else {
-        switch (month) {
-            case 1:
-            case 5:
-            case 3:
-            case 8:
-            case 10:
-            case 7:
-            case 12:
-                return 31 ;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                return 30;
-            default: return -1;
-        }
+            if (year % 400 == 0) {
+                return 29;
+            } else if (year % 100 == 0) {
+                return 28;
+            } else if (year % 4 == 0) {
+                return 29;
+            } else {
+                return 28;
+            }
+        else {
+            switch (month) {
+                case 1:
+                case 5:
+                case 3:
+                case 8:
+                case 10:
+                case 7:
+                case 12:
+                    return 31;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                default:
+                    return -1;
+            }
         }
     }
 
 
     /**
      * Средняя
-     *
+     * <p>
      * Проверить, лежит ли окружность с центром в (x1, y1) и радиусом r1 целиком внутри
      * окружности с центром в (x2, y2) и радиусом r2.
      * Вернуть true, если утверждение верно
      */
     boolean circleInside(
-           double x1,double y1,double r1,
-           double x2,double y2,double r2
+            double x1, double y1, double r1,
+            double x2, double y2, double r2
     ) {
         if (r2 < r1) {
             return false;
@@ -97,13 +98,15 @@ public class Logical {
 
     /**
      * Средняя
-     *
+     * <p>
      * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
      * Стороны отверстия должны быть параллельны граням кирпича.
      * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
      * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
      * Вернуть true, если кирпич пройдёт
      */
-    boolean brickPasses(int a,int b,int c,int r,int s)  {return a * b <= r * s || a * c <= r * s || c * b <= r * s;}
+    boolean brickPasses(int a, int b, int c, int r, int s) {
+        return a * b <= r * s || a * c <= r * s || c * b <= r * s;
+    }
 
 }
